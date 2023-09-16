01aa94468276da9470d04c85e6eb7d939f1b495f
ABHAY CHAVAN <75495921+abhay113@users.noreply.github.com>
2023-09-16 18:52:09 +0530

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Security.Cryptography;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public int keys = 0;
+    public GameObject door;
+    public GameObject win;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(speed*Time.deltaTime,0,0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+           
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0,-speed*Time.deltaTime,0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if(keys == 3)
+        {
+            Destroy(door);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {//bounce the wall
+        if (collision.gameObject.tag == "walls")
+        {
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+               
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(0, -speed * Time.deltaTime, 0);
+
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(0, speed * Time.deltaTime, 0);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
+        }
+
+
+
+        if (collision.gameObject.tag == "keys")
+        {
+            keys++;
+         
+            Destroy(collision.gameObject);
+        }
+
+
+        if (collision.gameObject.tag == "enemies")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        
+
+        if (collision.gameObject.tag == "win" )
+        {
+
+            Debug.Log( " You Won  !!!");
+
+        }
+
+
+
+
+    }
+}
