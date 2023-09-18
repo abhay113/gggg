af20d4726f160d673509c8b3ad1da5fb42f99856
ABHAY CHAVAN <75495921+abhay113@users.noreply.github.com>
2023-09-18 18:50:52 +0530

    added main

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 using System.Security.Cryptography;
 
 public class Player : MonoBehaviour
